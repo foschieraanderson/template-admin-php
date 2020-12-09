bd787c43026cb21a6b3cd92a5e7681e5d05a54f2
foschieraanderson <foschieraanderson@gmail.com>
2020-12-09 06:08:10 -0300

    Estrutura da página login

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
-<?php
-	echo 'Testando ambiente de desenvolvimento';
-?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+    <section class="container">
+        <div class="box-login">
+            <h1>Login</h1>
+            <form class="login" action="">
+                <input type="email" name="email" placeholder="E-mail">
+                <input type="password" name="password" placeholder="Senha">
+                <button type="submit">Entrar</button>
+            </form>
+        </div>
+    </section>
+</body>
+</html>
