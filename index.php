afaced749559bce1cbbd39cbdde9c4f7a36b2119
foschieraanderson <foschieraanderson@gmail.com>
2020-12-10 14:15:58 -0300

    Welcome user authenticated

--- index.php
+++ index.php
@@ -1,20 +1,15 @@
+<?php
+    include_once 'includes/conn.php';
+    include_once 'includes/authenticate.php';
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <title>Login</title>
-    <link rel="stylesheet" href="css/main.css">
+    <title>Painel Administrativo</title>
 </head>
 <body>
-    <section class="container">
-        <div class="box-login">
-            <h1>Login</h1>
-            <form class="login" action="">
-                <input type="email" name="email" placeholder="E-mail">
-                <input type="password" name="password" placeholder="Senha">
-                <button type="submit">Entrar</button>
-            </form>
-        </div>
-    </section>
+    <h1>Bem vindo, <?php echo $_SESSION['user'] ?></h1>
 </body>
 </html>
