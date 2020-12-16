00bbcb8c0eef494a0daaa0970095620097f1aabe
foschieraanderson <foschieraanderson@gmail.com>
2020-12-15 23:54:18 -0300

    Corrigindo lógica de acesso

--- includes/authenticate.php
+++ includes/authenticate.php
@@ -1,9 +1,9 @@
 <?php
 
-    include_once 'conn.php';
+    require_once 'conn.php';
     include_once 'path.php';
     
-    if(!isset($_SESSION['login']) || $_SESSION['login'] != true) {
+    if(!isset($_SESSION['login']) && $_SESSION['login'] != true) {
         header("Location: ".$path."login.php?erro=3");
     }
 
