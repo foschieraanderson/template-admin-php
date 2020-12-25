7dad20caece208ba968be622cff0f54b5e15b259
foschieraanderson <foschieraanderson@gmail.com>
2020-12-25 16:48:37 -0300

    Esboço menu.php

---
+++ app/view/menu.php
@@ -0,0 +1,10 @@
+<nav>
+    <div>Blog Panel</div>
+    <ul>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+    </ul>
+</nav>>
