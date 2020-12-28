9cced5d44f77fb9cc14ceb173465de76f1a4e5f4
foschieraanderson <foschieraanderson@gmail.com>
2020-12-28 14:13:09 -0300

    Estrutura inicial menu

--- app/view/menu.php
+++ app/view/menu.php
@@ -1,10 +1,9 @@
 <nav>
     <div>Blog Panel</div>
     <ul>
-        <li><a href=""></a></li>
-        <li><a href=""></a></li>
-        <li><a href=""></a></li>
-        <li><a href=""></a></li>
-        <li><a href=""></a></li>
+        <li><a href="">Dashboard</a></li>
+        <li><a href="">Notícias</a></li>
+        <li><a href="">Usuários</a></li>
+        <li><a href="">Configurações</a></li>
     </ul>
 </nav>>
