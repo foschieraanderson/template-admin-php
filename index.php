d02a9a7eddb06a959b7b0ddee302228e0d9fb65a
foschieraanderson <foschieraanderson@gmail.com>
2020-12-31 22:23:58 -0300

    Estrutura inicial dashboard

--- index.php
+++ index.php
@@ -9,9 +9,16 @@
 <head>
     <meta charset="UTF-8">
     <title>Painel Administrativo</title>
+    <link rel="stylesheet" href="public/css/main.css">
 </head>
 <body>
-    <?php include 'app/view/menu.php'; ?>
-    <?php include 'app/view/header.php'; ?>
+    <div class="container">
+        <div class="box-menu">
+            <?php include 'app/view/menu.php'; ?>
+        </div>
+        <div class="box-content">
+            <?php include 'app/view/header.php'; ?>            
+        </div>
+    </div>
 </body>
 </html>
